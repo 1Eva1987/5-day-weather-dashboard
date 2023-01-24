431e451c3802d46ce3ee94f66bd512a73f3648e7
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-01-24 17:38:50 +0000

    second API call created to get weather information , need fixing to not send request untill first API returns the values, needed values reached and logged in the console

--- script.js
+++ script.js
@@ -11,29 +11,49 @@ searchBtn.on("click", function (e) {
   e.preventDefault();
   locationName = inputEl.val().trim();
   console.log(locationName);
-  //   url to access locations lat and lon
+  latLon();
+  //   Base url with lon and lat
+
+  var baseURL =
+    " https://api.openweathermap.org/data/2.5/forecast?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    key;
+  $.ajax({
+    url: baseURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(baseURL);
+    console.log(response);
+    console.log(response.city.name);
+    console.log(response.list[0].dt_txt);
+    console.log(response.list[0].weather[0].icon);
+    console.log("Wind: " + response.list[0].wind.speed + " KPH");
+    console.log(
+      "Temp: " + (response.list[0].main.temp - 273.15).toFixed(0) + " C"
+    );
+    console.log("Humidity: " + response.list[0].main.humidity + " %");
+  });
+});
+
+// Function to get lon and lat
+function latLon() {
+  // url to access locations lat and lon
   var locationURL =
     "https://api.openweathermap.org/geo/1.0/direct?q=" +
     locationName +
     "&limit=5&appid=" +
     key;
-
+  // getting lat and lon values
   $.ajax({
     url: locationURL,
     method: "GET",
   }).then(function (response) {
-    console.log(locationURL);
-    console.log(response);
-    console.log(response[0].lat);
-    console.log(response[0].lon);
+    lat = response[0].lat;
+    lon = response[0].lon;
+    console.log(lat);
+    console.log(lon);
   });
-});
-
-// Base url with lon and lat
-// var baseURL =
-//   " https://api.openweathermap.org/data/2.5/forecast?lat=" +
-//   lat +
-//   "&lon=" +
-//   lon +
-//   "&appid=" +
-//   key;
+}

0c4ef015e96b70b8ffde378578b5bf590891a918
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-01-24 13:31:27 +0000

    Trying to make API call and log the response

--- script.js
+++ script.js
@@ -4,27 +4,34 @@ var searchBtn = $(".search-button");
 var key = "e5d92f08ca1eeb7ebd94f78928323033";
 var lat;
 var lon;
-var locationName;
+var locationName = "";
 
 // Get input value when user input city name
 searchBtn.on("click", function (e) {
   e.preventDefault();
   locationName = inputEl.val().trim();
   console.log(locationName);
+  //   url
+  var locationURL =
+    "https://api.openweathermap.org/geo/1.0/direct?q=" +
+    locationName +
+    "&limit=5&appid=" +
+    key;
+
+  $.ajax({
+    ulr: locationURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(locationURL);
+    // console.log(response);
+  });
 });
 
-// URL to to make an API call to get coordinates by location name
-var locationURL =
-  "http://api.openweathermap.org/geo/1.0/direct?q=" +
-  locationName +
-  "&limit=5&appid=" +
-  key;
-
 // Base url with lon and lat
-var baseURL =
-  " https://api.openweathermap.org/data/2.5/forecast?lat=" +
-  lat +
-  "&lon=" +
-  lon +
-  "&appid=" +
-  key;
+// var baseURL =
+//   " https://api.openweathermap.org/data/2.5/forecast?lat=" +
+//   lat +
+//   "&lon=" +
+//   lon +
+//   "&appid=" +
+//   key;
